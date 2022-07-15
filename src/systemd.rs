b614753cff485b036dd2e50631dde93c4bb60a5e
Lucy <lucy@absolucy.moe>
2022-07-15 11:06:29 -0400

    🩹 I genuinely forgot this was async. (Uses non-blocking systemd manager proxy)

--- src/systemd.rs
+++ src/systemd.rs
@@ -3,10 +3,12 @@
 use color_eyre::{eyre::WrapErr, Result};
 
 pub async fn start_systemd_target() -> Result<()> {
-	let manager = systemd_client::manager::build_blocking_proxy()
+	let manager = systemd_client::manager::build_nonblock_proxy()
+		.await
 		.wrap_err("failed to connect to org.freedesktop.systemd1.Manager")?;
 	manager
 		.start_unit("cosmic-session.target", "replace")
+		.await
 		.wrap_err("failed to start cosmic-session.target")?;
 	Ok(())
 }
