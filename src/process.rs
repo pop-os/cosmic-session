4c3e1cff0248473d7a21aa5be190ff1b718d0897
Ashley Wulber <ashley@system76.com>
2023-07-07 16:51:41 -0400

    cleanup: remove unused function

--- src/process.rs
+++ src/process.rs
@@ -17,18 +17,3 @@ pub(crate) fn mark_as_not_cloexec(file: &impl AsFd) -> Result<()> {
 	.wrap_err("failed to set CLOEXEC on file")?;
 	Ok(())
 }
-
-pub(crate) fn mark_as_cloexec(file: &impl AsFd) -> Result<()> {
-	let raw_fd = file.as_fd().as_raw_fd();
-	let fd_flags = fcntl::FdFlag::from_bits(
-		fcntl::fcntl(raw_fd, fcntl::FcntlArg::F_GETFD)
-			.wrap_err("failed to get GETFD value of stream")?,
-	)
-	.wrap_err("failed to get fd flags from file")?;
-	fcntl::fcntl(
-		raw_fd,
-		fcntl::FcntlArg::F_SETFD(fd_flags.union(fcntl::FdFlag::FD_CLOEXEC)),
-	)
-	.wrap_err("failed to set CLOEXEC on file")?;
-	Ok(())
-}
