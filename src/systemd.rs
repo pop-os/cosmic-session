abbfd9f4054e1a542307efbf196eba911b02ca8a
Daniel <hello@danielh.cc>
2024-08-21 13:07:20 -0400

    update

--- src/systemd.rs
+++ src/systemd.rs
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
+use std::path::Path;
 use std::process::{Command, Stdio};
 use std::sync::OnceLock;
 
@@ -33,7 +34,7 @@ pub fn stop_systemd_target() {
 ///Determine if systemd is used as the init system. This should work on all linux distributions.
 pub fn is_systemd_used() -> &'static bool {
 	static IS_SYSTEMD_USED: OnceLock<bool> = OnceLock::new();
-	IS_SYSTEMD_USED.get_or_init(|| std::fs::metadata("/run/systemd/system").is_ok())
+	IS_SYSTEMD_USED.get_or_init(|| Path::new("/run/systemd/system").exists())
 }
 
 #[cfg(feature = "systemd")]
